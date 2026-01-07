982621658a2c8c10040e2d23506980e13cb3819e
prabhu <prabhukadode1@gmail.com>
2026-01-07 23:39:50 +0530

    decorator

---
+++ python/decorator1.py
@@ -0,0 +1,14 @@
+def sayThankyou(func):
+    def wrapper(name,*args,**kwargs):
+        print("Started...")
+        func(name)
+        print("Thank you..")
+    return wrapper
+
+@sayThankyou
+def hello(name):
+    print("hello",name)
+
+hello("Prabhu")
+
+    
